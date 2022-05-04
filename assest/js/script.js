624342722d5552c0f0c1259398ba4153d74fb967
Valentina <valentina.paredes.11b@gmail.com>
2022-05-04 10:35:05 -0500

    add prueba

---
+++ assest/js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-active', windowPosition);
+})
+const optionMenu =document.querySelector(".select-menu"), 
+        selectBtn =optionMenu.querySelector(".select-btn"),
+        options= optionMenu.querySelector(".option"),
+        sBtn_text=optionMenu.querySelector(".sBtn-text");
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+options.forEach(option=>{
+    let selectedOption =option.querySelector(".option-text").innerText;
+    sBtn_text.innerText=selectedOption;
+
+})
