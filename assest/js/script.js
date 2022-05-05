cb6ddb1bd7eb4d181e158bdbd6e51166ea371dc6
Valentina <valentina.paredes.11b@gmail.com>
2022-05-05 12:52:08 -0500

    añadir cambios del navbar

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,11 +1,13 @@
 window.addEventListener('scroll', function () {
     let header = document.querySelector('header');
+    let search=document.querySelector(".search");
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('scrolling-active', windowPosition);
+    search.classList.toggle('search-desactive', windowPosition);
 })
 const optionMenu =document.querySelector(".select-menu"), 
         selectBtn =optionMenu.querySelector(".select-btn"),
-        options= optionMenu.querySelector(".option"),
+        options= optionMenu.querySelector(".options"),
         sBtn_text=optionMenu.querySelector(".sBtn-text");
 selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
 options.forEach(option=>{
