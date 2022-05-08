80994aa82e2df611b3eaa4bbdde21c734d569373
Manuel Bojaca <manuel.bojaca@outlook.com>
2022-05-08 15:03:16 -0500

    add all modules

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,22 +1,6 @@
 
-import scrollSnapPolyfill from 'css-scroll-snap-polyfill';
 
-const gra = function(min, max) {
-    return Math.random() * (max - min) + min;
-}
 
-const gri = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const init = function(){
-    let items = document.querySelectorAll('.modulex div');
-    for (let i = 0; i < items.length; i++){
-        items[i].style.minWidth = gra(30,60) + 'vw';
-    }
-    cssScrollSnapPolyfill();
-}
-init();
 
 
 window.addEventListener('scroll', function () {
