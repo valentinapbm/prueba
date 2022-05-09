27bda6fdbdcef7b156f9fd70c1abdfe315b154d8
Manuel Bojaca <manuel.bojaca@outlook.com>
2022-05-08 20:53:54 -0500

    add all

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,21 +1,16 @@
-
-
-
-
-
 window.addEventListener('scroll', function () {
     let header = document.querySelector('header');
+    let search=document.querySelector(".search");
+    let navList=document.querySelector(".nav-list1");
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('scrolling-active', windowPosition);
+    search.classList.toggle('search-desactive', windowPosition);
+    navList.classList.toggle('nav-list-desactive', windowPosition);
+
 })
 const optionMenu =document.querySelector(".select-menu"), 
         selectBtn =optionMenu.querySelector(".select-btn"),
-        options= optionMenu.querySelector(".option"),
+        options= optionMenu.querySelector(".options"),
         sBtn_text=optionMenu.querySelector(".sBtn-text");
 selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
-options.forEach(option=>{
-    let selectedOption =option.querySelector(".option-text").innerText;
-    sBtn_text.innerText=selectedOption;
-
-})
 
